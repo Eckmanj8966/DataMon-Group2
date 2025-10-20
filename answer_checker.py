ec68fb5005b506c1ffe276cc8f516d4b3d81f3ab
Joshua Eckman <124816827+Eckmanj8966@users.noreply.github.com>
2025-10-20 16:26:12 +0000

    Added Foundeli's answer_checker committed

--- answer_checker.py
+++ answer_checker.py
@@ -1,168 +1,51 @@
-# Answer Checker
-def answer_checker():
-    val_1 = None
-    val_2 = None
-    ans = None
-    guess = None
-    choice = None
-    
-    # Menu:
-    print("\nPlease choose the problem type of your choice:")
-    print("1) Addition +")
-    print("2) Subtraction -")
-    print("3) Multiplacation *")
-    print("4) Division /")
-    
-    # Get user choice
-    while choice is None:
+def get_number(prompt):
+    while True:
         try:
-            choice = int(input("Choice: "))
+            return float(input(prompt))
         except ValueError:
             print("Invalid input. Please enter a number.\n")
-    
-    # Menu choices:
-    # Addition
-    if choice == 1:
-        # Enter Value 1 and check that it is a number
-        while val_1 is None:
-            try:
-                val_1 = int(input("Please enter your first number: "))
-            except ValueError:
-                print("Invalid input. Please enter a number.\n")
-        
-        # Enter Value 2 and check that it is a number
-        while val_2 is None:
-            try:
-                val_2 = int(input("Please enter your second number: "))
-            except ValueError:
-                print("Invalid input. Please enter a number.\n")
-        
-        # Calculate answer
-        ans = val_1 + val_2
-        
-        # Get Guess
-        while True:
-            try:
-                guess = int(input(f"What is {val_1} + {val_2}? "))
-                break
-            except ValueError:
-                print("Invalid input. Please enter a number.\n")
-        
-        # Tell user if answer is correct
-        if guess == ans:
-            print(f"Correct! {val_1} + {val_2} = {ans}\n")
-        else:
-            print(f"I am sorry but that is incorrect. {val_1} + {val_2} = {ans}\n")
-        
-    # Subtraction
-    elif choice == 2:
-        # Enter Value 1 and check that it is a number
-        while val_1 is None:
-            try:
-                val_1 = int(input("Please enter your first number: "))
-            except ValueError:
-                print("Invalid input. Please enter a number.\n")
-        
-        # Enter Value 2 and check that it is a number
-        while val_2 is None:
-            try:
-                val_2 = int(input("Please enter your second number: "))
-            except ValueError:
-                print("Invalid input. Please enter a number.\n")
-        
-        # Calculate answer
-        ans = val_1 - val_2
-        
-        # Get Guess
-        while True:
-            try:
-                guess = int(input(f"What is {val_1} - {val_2}? "))
-                break
-            except ValueError:
-                print("Invalid input. Please enter a number.\n")
-        
-        # Tell user if answer is correct
-        if guess == ans:
-            print(f"Correct! {val_1} - {val_2} = {ans}\n")
-        else:
-            print(f"I am sorry but that is incorrect. {val_1} - {val_2} = {ans}\n")
-        
-    # Multiplacation
-    elif choice == 3:
-        # Enter Value 1 and check that it is a number
-        while val_1 is None:
-            try:
-                val_1 = int(input("Please enter your first number: "))
-            except ValueError:
-                print("Invalid input. Please enter a number.\n")
-        
-        # Enter Value 2 and check that it is a number
-        while val_2 is None:
-            try:
-                val_2 = int(input("Please enter your second number: "))
-            except ValueError:
-                print("Invalid input. Please enter a number.\n")
-        
-        # Convert Int to Float
-        fval_1 = float(val_1)
-        fval_2 = float(val_2)
-        
-        # Calculate answer
-        ans = fval_1 * fval_2
-        ans = round(ans, 1)
-        
-        # Get Guess
-        while True:
-            try:
-                print("Rounded to the first decimal place. (e.g. 10.5)")
-                guess = float(input(f"What is {val_1} * {val_2}? "))
-                break
-            except ValueError:
-                print("Invalid input. Please enter a number.\n")
-        
-        # Tell user if answer is correct
-        if guess == ans:
-            print(f"Correct! {val_1} * {val_2} = {ans}\n")
-        else:
-            print(f"I am sorry but that is incorrect. {val_1} * {val_2} = {ans}\n")
-        
-    # Division
-    elif choice == 4:
-        # Enter Value 1 and check that it is a number
-        while val_1 is None:
-            try:
-                val_1 = int(input("Please enter your first number: "))
-            except ValueError:
-                print("Invalid input. Please enter a number.\n")
-        
-        # Enter Value 2 and check that it is a number
-        while val_2 is None:
-            try:
-                val_2 = int(input("Please enter your second number: "))
-            except ValueError:
-                print("Invalid input. Please enter a number.\n")
-        
-        # Convert Int to Float
-        fval_1 = float(val_1)
-        fval_2 = float(val_2)
-        
-        # Calculate answer
-        ans = fval_1 / fval_2
-        ans = round(ans, 1)
-        
-        # Get Guess
-        while True:
-            try:
-                print("Rounded to the first decimal place. (e.g. 10.5)")
-                guess = float(input(f"What is {val_1} / {val_2}? "))
-                break
-            except ValueError:
-                print("Invalid input. Please enter a number.\n")
-        
-        # Tell user if answer is correct
-        if guess == ans:
-            print(f"Correct! {val_1} / {val_2} = {ans}\n")
-        else:
-            print(f"I am sorry but that is incorrect. {val_1} / {val_2} = {ans}\n")
-    
 
+
+def get_int_choice(prompt, min_choice, max_choice):
+    while True:
+        try:
+            choice = int(input(prompt))
+            if min_choice <= choice <= max_choice:
+                return choice
+            else:
+                print(f"Please enter a number between {min_choice} and {max_choice}.\n")
+        except ValueError:
+            print("Invalid input. Please enter a number.\n")
+
+
+def answer_checker():
+    print("\nPlease choose the problem type of your choice:")
+    print("1) Addition (+)")
+    print("2) Subtraction (-)")
+    print("3) Multiplication (*)")
+    print("4) Division (/)")
+
+    choice = get_int_choice("Choice: ", 1, 4)
+
+    val_1 = get_number("Please enter your first number: ")
+    val_2 = get_number("Please enter your second number: ")
+
+    operations = {
+        1: ("+", val_1 + val_2),
+        2: ("-", val_1 - val_2),
+        3: ("*", round(val_1 * val_2, 1)),
+        4: ("/", round(val_1 / val_2, 1))
+    }
+
+    op_symbol, correct_answer = operations[choice]
+
+    if choice in (3, 4):
+        print("Note: Answer will be rounded to the first decimal place. (e.g. 10.5)")
+
+    guess_prompt = f"What is {val_1} {op_symbol} {val_2}? "
+    guess = get_number(guess_prompt)
+
+    if guess == correct_answer:
+        print(f"Correct! {val_1} {op_symbol} {val_2} = {correct_answer}\n")
+    else:
+        print(f"I am sorry but that is incorrect. {val_1} {op_symbol} {val_2} = {correct_answer}\n")
